8f7171716a78966de2f51da2e208e76f685c96af
red <manishchoudhary0870@gmail.com>
2021-03-30 14:22:20 +0530

    ssh key corrected

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -12,6 +12,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCcJlEWX8Bwmwtvi+NXz0aul96kzcI72BHZ0M+SdNkpsGT3Jb3mb+knpTeHfgaPSxYERCPgTjw/qDAoEwxpIFeVowxF687LVtBm4mQtvF+6y+VFMLh87tesR+zXeP4k46V3e3CKA5elIAAEvrVz+nWFcumBu96WYGV4KaGofKHMrljqY5vkzEssP5tZLrgGWCmHvjO5vAUyVgYiLgeTcDq1ZupCttp8Zrf6+FwB4JeWAMiH36hXyBnKXz/8xWWNrI8MPdqlOvJpH1gpurb1av2UlSQJyuNUQcNy75o8ggCRg3JZN/t6spJkE390UCh72TrhxOLXeuYKAyUh3HV013dh root@master.puppet.vm',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCcJlEWX8Bwmwtvi+NXz0aul96kzcI72BHZ0M+SdNkpsGT3Jb3mb+knpTeHfgaPSxYERCPgTjw/qDAoEwxpIFeVowxF687LVtBm4mQtvF+6y+VFMLh87tesR+zXeP4k46V3e3CKA5elIAAEvrVz+nWFcumBu96WYGV4KaGofKHMrljqY5vkzEssP5tZLrgGWCmHvjO5vAUyVgYiLgeTcDq1ZupCttp8Zrf6+FwB4JeWAMiH36hXyBnKXz/8xWWNrI8MPdqlOvJpH1gpurb1av2UlSQJyuNUQcNy75o8ggCRg3JZN/t6spJkE390UCh72TrhxOLXeuYKAyUh3HV013dh',
   }
 }
